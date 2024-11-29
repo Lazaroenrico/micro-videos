cf6e4a832021205e3e92139722f558458c0e4a8d
Lazaroenrico <lazaro.enr.menezes@gmail.com>
2024-11-29 13:54:14 +0000

    shared/class-validator-fields: correcao de escrita

--- src/shared/domain/validators/class-validator-field.ts
+++ src/shared/domain/validators/class-validator-field.ts
@@ -1,10 +1,10 @@
 import { validateSync } from "class-validator";
-import { FieldsErros, IValidatorFields } from "./validator-fields-interface";
+import { FieldsErrors, IValidatorFields } from "./validator-fields-interface";
 
 export abstract class ClassValidatorFields<PropsValidated>
   implements IValidatorFields<PropsValidated>
 {
-  errors: FieldsErros | null = null;
+  errors: FieldsErrors | null = null;
   validatedData: PropsValidated | null = null;
 
   validate(data: any): boolean {
